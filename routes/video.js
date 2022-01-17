5891a3b5cd204b6c855b58eade64d6a486e3b26f
nishanbhat <rananishanbhat@gmail.com>
2022-01-17 19:42:27 +0545

    FFMPEG added to generate thumbnails

--- routes/video.js
+++ routes/video.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../models/User");
-
-const { auth } = require("../middleware/auth");
 const multer = require('multer');
+var ffmpeg = require('fluent-ffmpeg');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/')
+
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
     },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, `${Date.now()}_${file.originalname}`)
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`)
     },
     fileFilter: (req, file, cb) => {
         const ext = path.extname(file.originalname)
@@ -20,9 +19,10 @@ const storage = multer.diskStorage({
         }
         cb(null, true)
     }
-  })
-  
-  var upload = multer({ storage: storage }).single("file")
+})
+
+var upload = multer({ storage: storage }).single("file")
+
 
 //=================================
 //             User
@@ -38,8 +38,44 @@ router.post("/uploadfiles", (req, res) => {
         return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
     })
 
-    
 });
+
+
+router.post("/thumbnail", (req, res) => {
+
+    let thumbsFilePath ="";
+    let fileDuration ="";
+
+    ffmpeg.ffprobe(req.body.filePath, function(err, metadata){
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+
+        fileDuration = metadata.format.duration;
+        
+    })
+
+
+    ffmpeg(req.body.filePath)
+        .on('filenames', function (filenames) {
+            console.log('Will generate ' + filenames.join(', '))
+            thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+        })
+        .on('end', function () {
+            console.log('Screenshots taken');
+            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+        })
+        .screenshots({
+            // Will take screens at 20%, 40%, 60% and 80% of the video
+            count: 3,
+            folder: 'uploads/thumbnails',
+            size:'320x240',
+            // %b input basename ( filename w/o extension )
+            filename:'thumbnail-%b.png'
+        });
+
+});
+
+
 
 
 
