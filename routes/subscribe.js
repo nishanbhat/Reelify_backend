a67099b4c7980c189a4cdd77aa902ad9c5c2765a
nishanbhat <rananishanbhat@gmail.com>
2022-02-05 15:33:45 +0545

    fetching the data info about being subscriber or not

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -21,4 +21,20 @@ router.post("/subscribeNumber", (req, res) => {
 
 });
 
+router.post("/subscribed", (req, res) => {
+
+    Subscriber.find({ "userTo": req.body.userTo , "userFrom": req.body.userFrom })
+    .exec((err, subscribe) => {
+        if(err) return res.status(400).send(err)
+
+        let result = false;
+        if(subscribe.length !== 0) {
+            result = true
+        }
+
+        res.status(200).json({ success: true, subcribed: result  })
+    })
+
+});
+
 module.exports = router;
